a86e521da8396a133e2a109b300a565a5d5be84c
Rodion Glukhov <public.rodion@icloud.com>
2020-08-06 19:26:52 +0400

    Создание конфига в момент подгрузки модуля; расширение настроек

--- site_parser/__init__.py
+++ site_parser/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-from site_parser.settings import Config
+from site_parser.settings import config
 
 
 def create_application() -> Flask:
     application = Flask(__name__, instance_relative_config=True)
-    application.config.from_object(Config())
+    application.config.from_object(config)
 
     from site_parser import api
     application.register_blueprint(api.blueprint)
