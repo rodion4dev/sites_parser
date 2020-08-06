6bee797d2d725ea87c0810fdfc250cfb5c44aa48
Rodion Glukhov <public.rodion@icloud.com>
2020-08-06 18:49:41 +0400

    Хранение настроек из переменных окружения

---
+++ site_parser/settings.py
@@ -0,0 +1,8 @@
+import os
+
+
+class Config:
+    DEBUG: bool = os.environ.get('DEBUG', default='False').lower() == 'true'
+    SECRET_KEY: str = os.environ['SECRET_KEY']
+    REDIS_DATABASE_HOSTNAME: str = os.environ.get('REDIS_DATABASE_HOSTNAME')
+    REDIS_DATABASE_PORT: str = os.environ.get('REDIS_DATABASE_PORT')
