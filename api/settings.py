997737b69835660b293e0a363e00e133c9d34014
Rodion Glukhov <public.rodion@icloud.com>
2020-08-07 08:35:33 +0400

    Смена и добавление настроек

--- api/settings.py
+++ api/settings.py
@@ -1,9 +1,15 @@
 import os
 
 
-class Config:
+class _Config:
     DEBUG: bool = os.environ.get('DEBUG', default='False').lower() == 'true'
     SECRET_KEY: str = os.environ['SECRET_KEY']
+    REDIS_HOST: str = os.environ.get('REDIS_HOST')
+    REDIS_PORT: int = int(os.environ.get('REDIS_PORT', default='6379'))
+    REDIS_BROKER_DATABASE_NUMBER: int = int(
+        os.environ.get('REDIS_BROKER_DATABASE_NUMBER', default='0'))
+    REDIS_RESULTS_DATABASE_NUMBER: int = int(
+        os.environ.get('REDIS_RESULTS_DATABASE_NUMBER', default='0'))
 
 
-config = Config()
+config = _Config()
