24ebe435c95330d5d59995f8664ca5dbd6a10651
Rodion Glukhov <public.rodion@icloud.com>
2020-08-07 08:53:27 +0400

    Подготовка celery.py

--- tasks_call/celery.py
+++ tasks_call/celery.py
@@ -1,2 +1,18 @@
+import os
+
+from celery import Celery
+
+
 class Config:
-    pass
+    REDIS_HOST: str = os.environ.get('REDIS_HOST')
+    REDIS_PORT: int = int(os.environ.get('REDIS_PORT', default='6379'))
+    REDIS_BROKER_DATABASE_NUMBER: int = int(
+        os.environ.get('REDIS_BROKER_DATABASE_NUMBER', default='0'))
+
+
+redis_url_template = 'redis://{host}:{port}/{database_number}'
+redis_broker = redis_url_template.format(
+    host=Config.REDIS_HOST, port=Config.REDIS_PORT,
+    database_number=Config.REDIS_BROKER_DATABASE_NUMBER)
+application = Celery(main='tasks_call', broker=redis_broker,
+                     include=['tasks_call.tasks'])
