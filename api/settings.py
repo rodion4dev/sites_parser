97fdb7557fd351deba8c1d12962393760e6d82c1
Rodion Glukhov <public.rodion@icloud.com>
2020-08-07 11:23:18 +0400

    Обновление и добавление настроек

--- api/settings.py
+++ api/settings.py
@@ -4,12 +4,18 @@ import os
 class _Config:
     DEBUG: bool = os.environ.get('DEBUG', default='False').lower() == 'true'
     SECRET_KEY: str = os.environ['SECRET_KEY']
-    REDIS_HOST: str = os.environ.get('REDIS_HOST')
-    REDIS_PORT: int = int(os.environ.get('REDIS_PORT', default='6379'))
-    REDIS_BROKER_DATABASE_NUMBER: int = int(
-        os.environ.get('REDIS_BROKER_DATABASE_NUMBER', default='0'))
-    REDIS_RESULTS_DATABASE_NUMBER: int = int(
-        os.environ.get('REDIS_RESULTS_DATABASE_NUMBER', default='0'))
+    CELERY_BROKER: str = os.environ.get('CELERY_BROKER')
+    CELERY_BROKER_HOST: str = os.environ.get('CELERY_BROKER_HOST')
+    CELERY_BROKER_PORT: str = os.environ.get('CELERY_BROKER_PORT')
+    CELERY_BROKER_VIRTUAL_HOST: str = os.environ.get(
+        'CELERY_BROKER_VIRTUAL_HOST')
+    CELERY_RESULTS_BACKEND: str = os.environ.get('CELERY_RESULTS_BACKEND')
+    CELERY_RESULTS_BACKEND_HOST: str = os.environ.get(
+        'CELERY_RESULTS_BACKEND_HOST')
+    CELERY_RESULTS_BACKEND_PORT: str = os.environ.get(
+        'CELERY_RESULTS_BACKEND_PORT')
+    CELERY_RESULTS_BACKEND_VIRTUAL_HOST: str = os.environ.get(
+        'CELERY_RESULTS_BACKEND_VIRTUAL_HOST')
 
 
 config = _Config()
